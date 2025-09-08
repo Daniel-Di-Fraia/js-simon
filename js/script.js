942536477cdb12bdbb9f9e18f0a489874878a869
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-08 17:23:55 +0200

    finalizzazione form pagina + condizioni per verificare quali e quanti numeri ha indovinato

--- js/script.js
+++ js/script.js
@@ -16,10 +16,15 @@
 
 //selezioniamo l'elemento per il contdown della pagina
 const countDownPage = document.getElementById("countdown");
+
 //selezioniamo la lista dove usciranno in output i 5 numeri casuali
 const numberList = document.getElementById("numbers-list");
+
 //selezioniamo il form della pagina per le risposte allo scadere del timer
 const formRisposte = document.getElementById("answers-form");
+
+//selezioniamo il P dal DOM per il result
+const mioP = document.getElementById("message");
 
 //creo una variabile di riferimento per il timer del contdown
 let timer;
@@ -29,12 +34,12 @@ contdownStart(countDownPage);
 
 //ARRAY numeri casuali
 //creiamo una variabile array per i li
-arrayLi = [];
+let arrayLi = [];
 
 // inseriamo i tag li all interno della lista con un ciclo for
 for(let i = 0; i < 5; i++){
     //creiamo una variabile con un numero casuale al suo interno
-    const random = randomNum(0,100);
+    const random = randomNum(1,50);
     //condizione per pushare il numero random nell'array, se gia presente ripete il ciclo per farne un altro
     if (!arrayLi.includes(random)){
         arrayLi.push(random);
@@ -48,6 +53,38 @@ for(let i = 0; i < 5; i++){
     }   
 }
 
+//FORM RISPOSTE
+//gestione bottone conferma
+formRisposte.addEventListener("submit",
+    (event) => {
+
+        //blocco invio del form
+        event.preventDefault();
+
+        //richiamiamo gli input
+        const inputElement = document.querySelectorAll(".form-control");
+        // console.log(inputElement);
+
+        //creiamo un array vuoto per gli input inseriti
+        let arrayRisp = [];
+        for(let i = 0; i < 5; i++){
+            // inseriamo gli input all interno del nuovo arrayRisp
+            let currentNum = parseInt(inputElement[i].value);
+            // console.log(currentNum);
+            if(arrayLi.includes(currentNum)){
+                arrayRisp.push(currentNum);
+            }
+        }
+        if(arrayRisp.length === 5){
+            mioP.innerHTML = `Bravo! Hai indovinato tutti i numeri!`;
+        } else if(arrayRisp.length === 0){
+            mioP.innerHTML = `Non hai indovinato neanche un numero!`
+        } else
+            mioP.innerHTML = `Hai indovinato ${arrayRisp.length} numeri! e sono: ${arrayRisp}`;
+        
+    }
+);
+
 
 
 //FUNZIONI
@@ -55,13 +92,16 @@ for(let i = 0; i < 5; i++){
 //creiamo una funzione per il contdown in pagina
 function contdownStart(contatore){
     //variabile di conteggio
-    let count = 30;
+    let count = 1;
 
     //incrementiamo la variabile count e facciamo l'output in pagina
     timer = setInterval(()=> {
         countDownPage.innerHTML = count--;
         if(count === -1){
             clearInterval(timer);
+            countDownPage.classList.add("d-none");
+            numberList.classList.add("d-none");
+            formRisposte.classList.remove("d-none");
         }
     }, 1000);
 }
