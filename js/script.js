ecc52556a0bd178c7be771b2075b87d59ebcc22f
Daniel-Di-Fraia <daniel.difraia2600@gmail.com>
2025-09-08 15:26:13 +0200

    finalizzazione array numeri univoci e casuali nella lista

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@
 
 
 
+//COUNTDOWN
 
 //selezioniamo l'elemento per il contdown della pagina
 const countDownPage = document.getElementById("countdown");
-
 
 //creo una variabile di riferimento per il timer del contdown
 let timer;
@@ -23,15 +23,10 @@ let timer;
 //richiamiamo la funzione per il contdown 
 contdownStart(countDownPage);
 
-
-
-
-
-
 //creiamo una funzione per il contdown in pagina
 function contdownStart(contatore){
     //variabile di conteggio
-    let count = 5;
+    let count = 30;
 
     //incrementiamo la variabile count e facciamo l'output in pagina
     timer = setInterval(()=> {
@@ -41,3 +36,38 @@ function contdownStart(contatore){
         }
     }, 1000);
 }
+
+//ARRAY numeri casuali
+
+//selezioniamo la lista dove usciranno in output i 5 numeri casuali
+const numberList = document.getElementById("numbers-list");
+
+//creiamo una variabile array per i li
+arrayLi = [];
+
+// inseriamo i tag li all interno della lista con un ciclo for
+for(let i = 0; i < 5; i++){
+    //creiamo una variabile con un numero casuale al suo interno
+    const random = randomNum(0,100);
+    //condizione per pushare il numero random nell'array, se gia presente ripete il ciclo per farne un altro
+    if (!arrayLi.includes(random)){
+        arrayLi.push(random);
+        //creiamo i li nella lista con i numeri random
+        const newLi = document.createElement('li');
+        newLi.innerHTML = random;
+        numberList.appendChild(newLi);
+        //ALTRIMENTI ripeti il ciclo
+    } else {
+        i--;
+    }   
+}
+
+//creiamo una funzione per generare un numero random da 1 a 100
+function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
+
